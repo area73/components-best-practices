4e871dd837f4cc3908784f2cc913d8b1a1bfb006
rerades <rodrigo.erades@belvo.com>
2023-02-18 11:58:11 +0100

    adding use the platform

--- src/components/bvComposedListOk.vue
+++ src/components/bvComposedListOk.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts"></script>
 <template>
   <li>
-    <span class="pr-4"><slot name="leading"></slot></span>
-    <span><slot name="trailing"></slot></span>
+    <span class="pr-4"><slot name="leading" /></span>
+    <span><slot name="trailing" /></span>
   </li>
 </template>
 <style></style>
