41bca676dd6915e223cf534bc7ae3f2d8750d4e5
rerades <rerades@siete3.com>
2023-03-05 13:24:23 +0100

    final release

--- src/components/bvComposedListOk.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts"></script>
-<template>
-  <li>
-    <span class="pr-4"><slot name="leading" /></span>
-    <span><slot name="trailing" /></span>
-  </li>
-</template>
-<style></style>
