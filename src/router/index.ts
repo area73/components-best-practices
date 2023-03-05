f46afd409254c77840e2ddb48368ac59dea49430
rerades <rerades@siete3.com>
2023-03-05 12:18:21 +0100

    adding styles

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,19 @@
-import SingleElementPattern from "@/views/SingleElementPattern.vue";
-import UseThePaltform from "@/views/UseThePaltform.vue";
+import BibliographyResources from "@/views/BibliographyResources.vue";
+import SingleElementPattern from "@/views/HandsOnCode.vue";
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/usar-la-plataforma",
-      name: "useThePlatform",
-      component: UseThePaltform,
-    },
-    {
-      path: "/singel",
-      name: "singelElementPattern",
+      path: "/hands-on-code",
+      name: "handsOnCode",
       component: SingleElementPattern,
+    },
+    {
+      path: "/bibliography",
+      name: "bibliography",
+      component: BibliographyResources,
     },
   ],
 });
